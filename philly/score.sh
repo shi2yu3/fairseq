adb3898468274715bbd2f64f4e6f781506a55abd
shi2y <shi2yu3@live.com>
2019-05-05 22:19:09 +0800

    cluster as variable

--- philly/score.sh
+++ philly/score.sh
@@ -2,19 +2,20 @@
 
 set -x
 
-#cd `dirname $0`
-
 phillyfs=philly/philly-fs.bash
+cluster=wu3
+vc=msrmt
 
 
 for var in "$@"
 do
     id=$var
     sudo rm -r results/*
-    sudo bash $phillyfs -cp //philly/eu2/ipgsrch/sys/jobs/application_$id/candidate results/candidate
-    sudo bash $phillyfs -cp //philly/eu2/ipgsrch/sys/jobs/application_$id/gold results/gold
+    sudo bash $phillyfs -cp //philly/wu3/msrmt/sys/jobs/application_$id/candidate results/candidate
+    sudo bash $phillyfs -cp //philly/wu3/msrmt/sys/jobs/application_$id/gold results/gold
     ls -l results/
     res=$(docker run --rm -it -v $(pwd):/workspace bertsum /bin/bash -c "pyrouge_set_rouge_path examples/summarization/BertSum/pyrouge/tools/ROUGE-1.5.5 && python examples/summarization/BertSum/src/rouge.py")
+    echo $id
     echo $res
 done
 
