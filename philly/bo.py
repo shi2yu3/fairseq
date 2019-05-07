a5fc08bf34ffa2f0ed697a9ab43f8e9cdd945fcc
Yu Shi <yushi@microsoft.com>
2019-05-07 18:35:46 +0800

    support resume from stopped search

--- philly/bo.py
+++ philly/bo.py
@@ -8,6 +8,7 @@ import uuid
 import shlex
 import copy
 import subprocess
+import glob
 
 from bayes_opt import BayesianOptimization
 from bayes_opt.util import UtilityFunction, Colours
@@ -35,9 +36,11 @@ b_color = ["on_grey", "on_red", "on_green", "on_yellow", "on_blue", "on_magenta"
 parser = argparse.ArgumentParser()
 parser.add_argument("--num_threads", default=1, type=int,
                     help="number of parallel threads")
-args, config_file = parser.parse_known_args()
-assert len(config_file) == 1
-config_file = config_file[0]
+parser.add_argument("--localhost", default=9009, type=int,
+                    help="the local host")
+args, input_config_file = parser.parse_known_args()
+assert len(input_config_file) == 1
+input_config_file = input_config_file[0]
 
 
 def load_config(config_file):
@@ -59,7 +62,7 @@ def load_config(config_file):
     return dir, philly_config, pbounds
 
 
-root_dir, philly_config, pbounds = load_config(config_file)
+root_dir, philly_config, pbounds = load_config(input_config_file)
 
 
 def black_box_function(x, y):
@@ -99,10 +102,14 @@ def create_config_file(config_file, philly_config, **kwargs):
     args_in_command = parse_command(config["resources"]["workers"]["commandLine"])
 
     for arg in kwargs:
+        val = kwargs[arg]
+        if arg == "--warmup-updates":
+            val = int(val)
+
         if arg in config["environmentVariables"]:
-            config["environmentVariables"][arg] = kwargs[arg]
+            config["environmentVariables"][arg] = val
         elif arg in args_in_command:
-            args_in_command[arg] = kwargs[arg]
+            args_in_command[arg] = val
         else:
             raise ValueError(f"Argument {arg} is not used in command")
     commandLine = []
@@ -117,21 +124,21 @@ def create_config_file(config_file, philly_config, **kwargs):
             commandLine.append(str(v))
     config["resources"]["workers"]["commandLine"] = " ".join(commandLine)
 
-    json.dump(config, open(config_file, "w"), indent=2)
+    json.dump(config, open(config_file, "w"), indent=4)
 
 
 def submit_job(config_fn):
-    cmd = f'curl --ntlm --user : -X POST -H "Content-Type: application/json" --data @{config_fn} https://philly/api/jobs'
+    cmd = f'curl --silent --ntlm --user : -X POST -H "Content-Type: application/json" --data @{config_fn} https://philly/api/jobs'
     job_id = subprocess.check_output(cmd)
     # job_id = b'{"jobId":"application_1553675282044_3023"}'
-    print(job_id)
+    print(f"new job submitted {job_id}")
     job_id = job_id.decode("utf-8").replace("true", "True").replace("false", "False")
     job_id = eval(job_id)["jobId"].replace("application_", "")
     return job_id
 
 
 def job_status(job_id, cluster, vc):
-    cmd = f'curl -k --ntlm --user : "https://philly/api/status?clusterId={cluster}&vcId={vc}&jobId=application_{job_id}&jobType=cust&content=full"'
+    cmd = f'curl -k --silent --ntlm --user : "https://philly/api/status?clusterId={cluster}&vcId={vc}&jobId=application_{job_id}&jobType=cust&content=full"'
     while True:
         status = subprocess.check_output(cmd)
         status = status.decode("utf-8").replace("true", "True").replace("false", "False").replace("null", "None")
@@ -140,7 +147,6 @@ def job_status(job_id, cluster, vc):
             break
         except:
             print(status)
-            # raise
             time.sleep(60)
     if status["status"] == "Pass" or status["status"] == "Killed":
         return "finished"
@@ -150,7 +156,7 @@ def job_status(job_id, cluster, vc):
 
 
 def val_loss(job_id, cluster, vc):
-    best_loss = 1001.0
+    best_loss = None
     stdout_num = 1
     retry = 0
     while True:
@@ -158,11 +164,11 @@ def val_loss(job_id, cluster, vc):
         stdout = requests.get(stdout)
         if stdout.status_code == 200:
             for line in stdout.text.split("\n"):
-                if 'valid on "valid" subset' in line:
+                if "valid on 'valid' subset" in line:
                     segs = line.split(" | ")
                     epoch = str(int(segs[1].split()[1]))
                     loss = segs[3].split()[1]
-                    if float(loss) < best_loss:
+                    if best_loss is None or float(loss) < best_loss:
                         best_loss = float(loss)
                         best_epoch = epoch
                         print(f"epoch {best_epoch} best loss {best_loss}")
@@ -174,8 +180,42 @@ def val_loss(job_id, cluster, vc):
     return best_loss
 
 
+def wait_for(job_id, cluster, vc, trial_id, **kwargs):
+    info_file = os.path.join(root_dir, f"{trial_id}_info.json")
+
+    job_info = {"params": kwargs,
+                "trial_id": trial_id,
+                "philly_id": job_id,
+                "cluster": cluster,
+                "vc": vc,
+                "status": "running",
+                "target": None}
+
+    while True:
+        status = job_status(job_id, cluster, vc)
+        if status == "finished":
+            loss = val_loss(job_id, cluster, vc)
+            if loss is not None:
+                job_info["target"] = -loss
+            job_info["status"] = "finished"
+            json.dump(job_info, open(info_file, "w"), indent=4)
+            break
+        elif status == "running":
+            print(f"job {job_id} is still running")
+            json.dump(job_info, open(info_file, "w"), indent=4)
+            time.sleep(1800)
+        else:
+            job_info["status"] = "failed"
+            json.dump(job_info, open(info_file, "w"), indent=4)
+            break
+
+    return job_info["target"]
+
+
 def philly_job(philly_config, **kwargs):
-    config_file = os.path.join(root_dir, f"{str(uuid.uuid4()).split('-')[0]}.json")
+    trial_id = str(uuid.uuid4()).split('-')[0]
+
+    config_file = os.path.join(root_dir, f"{trial_id}_job.json")
     create_config_file(config_file, philly_config, **kwargs)
 
     cluster = philly_config["metadata"]["cluster"]
@@ -183,29 +223,7 @@ def philly_job(philly_config, **kwargs):
 
     job_id = submit_job(config_file)
 
-    while True:
-        status = job_status(job_id, cluster, vc)
-        if status == "finished":
-            return -val_loss(job_id, cluster, vc)
-        elif status == "running":
-            time.sleep(1800)
-        else:
-            return None
-
-
-def load_config(config_file):
-    if os.path.isdir(config_file):
-        config_file = os.path.join(config_file, "job.json")
-        config = json.load(open(config_file))
-    else:
-        dir = os.path.join("experiment", str(uuid.uuid4()).split("-")[0])
-        os.makedirs(dir, exist_ok=True)
-
-        config = json.load(open(config_file))
-
-        config_file = os.path.join(dir, "job.json")
-        json.dump(config, open(config_file, "w"), indent=4)
-    return config
+    return wait_for(job_id, cluster, vc, trial_id, **kwargs)
 
 
 class BayesianOptimizationHandler(RequestHandler):
@@ -243,63 +261,121 @@ def run_optimization_app():
     server = tornado.httpserver.HTTPServer(
         tornado.web.Application(handlers)
     )
-    server.listen(9009)
+    server.listen(args.localhost)
     tornado.ioloop.IOLoop.instance().start()
 
 
 def run_optimizer():
     global optimizers_config
     opt_config = optimizers_config.pop()
-    philly_config = opt_config["philly_config"]
     name = opt_config["name"]
     colour_f = opt_config["colour_f"]
     colour_b = opt_config["colour_b"]
 
     register_data = {}
     max_target = None
-    for _ in range(10):
+    if "philly_config" in opt_config:
+        philly_config = opt_config["philly_config"]
+
+        for _ in range(10):
+            status = name + " wants to register: {}.\n".format(register_data)
+
+            resp = requests.post(
+                url=f"http://localhost:{args.localhost}/bayesian_optimization",
+                json=register_data,
+            ).json()
+            target = philly_job(philly_config, **resp)
+
+            if target is None:
+                register_data = {}
+
+                status += name + " haven't got target.\n"
+            else:
+                register_data = {
+                    "params": resp,
+                    "target": target,
+                }
+
+                if max_target is None or target > max_target:
+                    max_target = target
+
+                status += name + " got {} as target.\n".format(target)
+
+            status += name + " will to register next: {}.\n".format(register_data)
+            print(colored(status, colour_f, colour_b), end="\n")
+    elif "job_info" in opt_config:
+        job_info = opt_config["job_info"]
+
         status = name + " wants to register: {}.\n".format(register_data)
 
-        resp = requests.post(
-            url="http://localhost:9009/bayesian_optimization",
+        requests.post(
+            url=f"http://localhost:{args.localhost}/bayesian_optimization",
             json=register_data,
-        ).json()
-        target = philly_job(philly_config, **resp)
+        )
 
-        register_data = {
-            "params": resp,
-            "target": target,
-        }
+        target = job_info["target"]
+        if target is not None:
+            register_data = {
+                "params": job_info["params"],
+                "target": target,
+            }
+        elif job_info["status"] != "failed":
+            target = wait_for(job_info["philly_id"],
+                              job_info["cluster"],
+                              job_info["vc"],
+                              job_info["trial_id"],
+                              **job_info["params"])
 
-        if max_target is None or target > max_target:
-            max_target = target
+            if target is None:
+                register_data = {}
+                status += name + " haven't got target.\n"
+            else:
+                register_data = {
+                    "params": job_info["params"],
+                    "target": target,
+                }
 
-        status += name + " got {} as target.\n".format(target)
+            if max_target is None or target > max_target:
+                max_target = target
+
+            status += name + " got {} as target.\n".format(target)
         status += name + " will to register next: {}.\n".format(register_data)
-        # print(colour(status), end="\n")
         print(colored(status, colour_f, colour_b), end="\n")
+    else:
+        raise ValueError("Optimizers config should contain 'philly_config' or 'job_info'")
 
-    global results
+
+    # global results
     results.append((name, max_target))
-    # print(colour(name + " is done!"), end="\n\n")
     print(colored(name + " is done!", colour_f, colour_b), end="\n\n")
 
+
+def resume_job_info(config_file):
+    job_info = []
+    if os.path.isdir(config_file):
+        dir = config_file
+        info_files = glob.glob(f"{dir}/*_info.json")
+        for info_file in info_files:
+            job_info.append(json.load(open(info_file)))
+    return job_info
+
+results = []
+
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("--num_threads", default=1, type=int,
-    #                     help="number of parallel threads")
-    # args = parser.parse_args()
-    # args, config_file = parser.parse_known_args()
-    # assert len(config_file) == 1
-    # config_file = config_file[0]
-
-    # philly_config = Experiment(config_file)
-
-    init()  # color
+    init()  # init color
     ioloop = tornado.ioloop.IOLoop.instance()
+
     optimizers_config = []
+
+    existing_jobs = resume_job_info(input_config_file)
+    for i, job_info in enumerate(existing_jobs):
+        if job_info["status"] != "failed":
+            optimizers_config.append({"job_info": job_info,
+                                      "name": f"optimizer {job_info['trial_id']}",
+                                      "colour_f": f_color[(i + args.num_threads) % len(f_color)],
+                                      "colour_b": "on_white"})
+
     for i in range(args.num_threads):
-        # optimizers_config.append({"name": f"optimizer {i}", "colour": Colours.red})  # Colours.green, Colours.blue
         optimizers_config.append({"philly_config": philly_config,
                                   "name": f"optimizer {i}",
                                   "colour_f": f_color[i % len(f_color)],
@@ -309,7 +385,7 @@ if __name__ == "__main__":
     app_thread.daemon = True
     app_thread.start()
 
-    targets = [run_optimizer] * args.num_threads
+    targets = [run_optimizer] * len(optimizers_config)
 
     optimizer_threads = []
     for target in targets:
@@ -317,7 +393,7 @@ if __name__ == "__main__":
         optimizer_threads[-1].daemon = True
         optimizer_threads[-1].start()
 
-    results = []
+
     for optimizer_thread in optimizer_threads:
         optimizer_thread.join()
 
